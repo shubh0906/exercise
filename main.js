006fc38fa67f522a9fce3c164d02318ba1880bef
shubh0906 <shubham_0906@yahoo.com>
2018-03-13 11:56:12 -0400

    Created hash.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
 const request = require('request');
+const {SHA256} = require('crypto-js');
 
 const email = "shubgupta@cs.stonybrook.edu";
 
 request('https://truveris.github.io/jobs/',(error,response,body) => {
 	if(!error && response.statusCode == 200)
-		console.log(body+email);
+		console.log(SHA256(body + email).toString());
 });
